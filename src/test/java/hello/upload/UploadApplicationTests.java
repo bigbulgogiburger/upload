ead3ccce8d5d4c46f7c1550b7d2c6d955059125d
pyundohoon <dhpyun@ilinker.co.kr>
2023-03-16 18:09:24 +0900

    added

--- src/test/java/hello/upload/UploadApplicationTests.java
+++ src/test/java/hello/upload/UploadApplicationTests.java
@@ -3,11 +3,42 @@ package hello.upload;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootTest
 class UploadApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void test(){
+		int[] height ={1,8,6,2,5,4,8,3,7};
+//		List<Integer> a = new ArrayList<>();
+//
+//		int[] round = new int[height.length];
+
+//		for(int i=0;i<height.length;i++){
+//			round[i] = (i+1)*height[i];
+//		}
+//
+//		int result = 0;
+//
+//		for(int i = height.length-1; i>=0; i--){
+//			int currentHeight= height[i];
+//			for(int k=0; k<i;k++){
+//				if(height[k]>=currentHeight){
+//
+//					if(result<currentHeight*(i-k)){
+//						result = currentHeight*(i-k);
+//					}
+//				}
+//			}
+//		}
+//		System.out.println(result);
+
 	}
 
 }
